d32b316647a47a0789d268b06757e494b56c73ee
mikkel41 <126462977+mikkel41@users.noreply.github.com>
2025-04-24 01:44:58 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Strauss'
+description 'Watermark´'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/logo.png'
+}
+
+client_script 'client.lua'
